75e508d715d3c797328858ec9d73443109b70fe6
Srivatsav Erramilli <srivatsaverramilli@gmail.com>
2024-02-14 15:56:21 -0700

    Initial commit of the Socket Project

---
+++ DHT_manager.py
@@ -0,0 +1,9 @@
+''' This python file acts as the DHT manager for the CSE 434 Socket Project. 
+    It is responsible for handling the DHT and communication between the peers.
+
+    The requirements for the milestone are as follows:
+    For the milestone deadline, you are to implement the following commands to the manager: register, setup-dht,
+    and dht-complete. This also involves implementation of commands that may be issued among peers associated
+    with these commands;    
+'''
+
---
+++ README.md
@@ -0,0 +1,5 @@
+CSE 434 Socket Project
+Socket Group 82
+Team members - Srivatsav Erramilli, Rithwika Annamaneedu
+
+This README file contains all the documentation for the socket project that is being created for the Socket Project assignment for the CSE 434 class in Spring 2024.
